2afc002bfaf529de1b62199444ebb5f1a2d46c8d
Luana Freitas <luanacassiaf@gmail.com>
2022-03-25 17:17:49 -0300

    Aula 07

---
+++ Aulas/07-constantes.php
@@ -0,0 +1,23 @@
+<?php
+// Constantes
+define("NOME", "Yuri");
+define("IDADE", 1);
+
+define("POKEMON", ['Charmander', 'Squitle', 'Bulbasaur']);
+
+echo NOME;
+var_dump(NOME);
+echo 'O gato ' . NOME . ' tem ' . IDADE . ' ano.';
+echo '<hr>';
+
+echo POKEMON[2];
+var_dump(POKEMON);
+
+// Constantes são globais
+
+function exibeNome()
+{
+    echo NOME;
+}
+
+exibeNome();
