255c3e3e4dd1db1e8ebea4a57fa1be5f14f82a1c
Luana Freitas <luanacassiaf@gmail.com>
2022-03-28 20:01:10 -0300

    Aula 08

---
+++ Aulas/08-arrays.php
@@ -0,0 +1,70 @@
+<?php
+// Arrays
+$carros = array("BMW", "Audi", "Hilux");
+
+echo $carros[0];
+echo '<br>';
+
+$carros[] = "Amarok";
+$carros[10] = "Ferrari";
+
+print_r($carros);
+echo '<hr>';
+
+$motos = ["Yamaha", "Honda", "Suzuki"];
+print_r($motos);
+echo '<hr>';
+
+// Count
+echo count($motos);
+echo '<hr>';
+
+// Foreach
+foreach($carros as $carro) {
+    echo $carro . '<br>';
+}
+echo '<hr>';
+
+// Arrays associativos
+$pessoa = array('nome' => 'Luana', 'idade' => 22, 'altura' => 1.6);
+echo $pessoa['nome'];
+echo '<br>';
+
+$pessoa['cidade'] = 'Townsville';
+print_r($pessoa);
+echo '<hr>';
+
+foreach($pessoa as $indice => $valor) {
+    echo $indice . ": " . $valor . "<br>";
+}
+echo '<hr>';
+
+// Arrays multidimensionais
+$evolucoes = array('Charmander' => array('Primeira evolução' => 'Charmeleon', 'Segunda evolução' => 'Charizard'),
+                    'Squirtle' => array('Primeira evolução' => 'Wartotle', 'Segunda evolução' => 'Blastoise'),
+                    'Bulbasaur' => array('Primeira evolução' => 'Ivysaur', 'Segunda evolução' => 'Venusaur')
+                );
+echo $evolucoes['Charmander']['Primeira evolução'];
+echo '<br>'; 
+
+foreach($evolucoes as $indice => $valor) {
+    echo $indice . '<br>';
+}
+echo '<br>'; 
+
+echo 'Evoluções de Charmander: <br>';
+foreach($evolucoes['Charmander'] as $indice => $valor) {
+    echo "$indice: $valor <br>";
+}
+echo '<br>'; 
+
+echo 'Evoluções de Squirtle: <br>';
+foreach($evolucoes['Squirtle'] as $indice => $valor) {
+    echo "$indice: $valor <br>";
+}
+echo '<br>'; 
+
+echo 'Evoluções de Bulbasaur: <br>';
+foreach($evolucoes['Bulbasaur'] as $indice => $valor) {
+    echo "$indice: $valor <br>";
+}
