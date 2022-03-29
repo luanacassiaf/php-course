6c7ce20d83594a88c8eaac83e77de01b50952fb6
Luana Freitas <luanacassiaf@gmail.com>
2022-03-29 16:56:00 -0300

    Funções de array

---
+++ Aulas/09-funcoes-de-arrays.php
@@ -0,0 +1,78 @@
+<?php
+$pokemons = array("Fogo" => "Charmander", "Água" => "Squirtle", "Planta" => "Bulbasaur");
+
+if(is_array($pokemons)):
+    echo "É um array. <br>";
+else:
+    echo "Não é array. <br>";
+endif;
+
+if(in_array("Pichu", $pokemons)):
+    echo "Existe no array. <br>";
+else:
+    echo "Não existe no array. <br>";
+endif;
+echo '<hr>';
+
+// Índices do array pokemons são valores do array keys
+$keys = array_keys($pokemons);
+print_r($keys);
+echo '<br>';
+
+// Valores do array pokemons são valores do array values
+$values = array_values($pokemons);
+print_r($values);
+echo '<hr>';
+
+$agua = array("Squirtle", "Totodile", "Piplup");
+$fogo = array("Charmander", "Cyndaquil", "Torchic");
+
+// União dos elementos de dois arrays
+$uniao = array_merge($agua, $fogo);
+print_r($uniao);
+echo '<hr>';
+
+// Apaga elementos do final do array
+echo array_pop($uniao) . '<br>';
+print_r($uniao);
+echo '<hr>';
+
+// Apaga elementos do início do array
+echo array_shift($uniao) . '<br>';
+print_r($uniao);
+echo '<hr>';
+
+// Adiciona elementos no início do array
+array_unshift($uniao, "Pikachu", "Chikorita");
+print_r($uniao);
+echo '<hr>';
+
+// Adiciona elementos no final do array
+array_push($uniao, "Eevee");
+print_r($uniao);
+echo '<hr>';
+
+// Cria um array utilizando um array para chaves e outro para valores
+print_r($keys);
+echo '<br>';
+print_r($values);
+echo '<br>';
+$equipe = array_combine($keys, $values);
+print_r($equipe);
+echo '<hr>';
+
+// Soma elementos numéricos de array
+$soma = array(5.5, 4.4, 3.3 );
+echo array_sum($soma);
+echo '<hr>';
+
+// Transforma string em array
+$data = "02/10/2020";
+$novaData = explode("/", $data);
+print_r($novaData);
+echo '<hr>';
+
+$passaros = array("Moltres", "Zapdos", "Articuno");
+$string = implode(", ", $passaros);
+echo $string;
+echo '<hr>';
