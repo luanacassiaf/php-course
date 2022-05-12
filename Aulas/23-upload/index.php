aa36e601a59ef84a7bd212898ec8e1d660773b10
Luana Freitas <luanacassiaf@gmail.com>
2022-05-12 20:29:35 -0300

    Uploads múltiplos

--- Aulas/23-upload/index.php
+++ Aulas/23-upload/index.php
@@ -10,29 +10,35 @@
 
 <?php
     if(isset($_POST['enviar'])):
-        $formatosPermitidos = array("png", "jpeg", "jpg", "gif");
-        $extensao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
         
-        if(in_array($extensao, $formatosPermitidos)):
-            $pasta = "arquivos/";
-            $temp = $_FILES['arquivo']['tmp_name'];
-            $novoNome = uniqid().".$extensao";
+        $formatosPermitidos = array("png", "jpeg", "jpg", "gif");
+        $numArquivos = count($_FILES['arquivo']['name']);
+        $contador = 0;
 
-            if(move_uploaded_file($temp, $pasta.$novoNome)):
-                $mensagem = "Upload feito com sucesso.";
+        while ($contador < $numArquivos):
+            $extensao = pathinfo($_FILES['arquivo']['name'][$contador], PATHINFO_EXTENSION);
+            
+            if(in_array($extensao, $formatosPermitidos)):
+                $pasta = "arquivos/";
+                $temp = $_FILES['arquivo']['tmp_name'][$contador];
+                $novoNome = uniqid().".$extensao";
+
+                if(move_uploaded_file($temp, $pasta.$novoNome)):
+                    echo "Upload feito com sucesso em $pasta.$novoNome. <br>";
+                else:
+                    echo "Erro ao enviar arquivo $temp. <br>";
+                endif;
             else:
-                $mensagem = "Erro.";
+                echo "Extensão $extensao não permitida. <br>";
             endif;
-        else:
-            $mensagem = "Formato inválido.";
-        endif;
 
-        echo $mensagem;
+            $contador++;
+        endwhile;
     endif;
 ?>
 
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
-        <input type="file" name="arquivo">
+        <input type="file" name="arquivo[]" multiple>
         <input type="submit" name="enviar">
     </form>
 </body>
