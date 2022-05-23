1ab2d364915d2f3b50f26d81d3ed014048eed01f
Luana Freitas <luanacassiaf@gmail.com>
2022-05-23 12:11:43 -0300

    Sessões e login

---
+++ Aulas/25-sistema-login/index.php
@@ -0,0 +1,62 @@
+<?php
+    require_once 'db_connect.php';
+    session_start();
+
+    if (isset($_POST['entrar'])) {
+        $erros = [];
+        $login = mysqli_escape_string($connect, $_POST['login']);
+        $senha = mysqli_escape_string($connect, $_POST['senha']);
+
+        if (empty($login) or empty($senha)) {
+            $erros[] = '<li>O campo login/senha precisa ser preenchido.</li>';
+        } else {
+            $sql = "SELECT login FROM usuarios WHERE login = '{$login}'";
+            $resultado = mysqli_query($connect, $sql);
+
+            if (mysqli_num_rows($resultado) > 0) {
+                $senha = md5($senha);
+                $sql = "SELECT * FROM usuarios WHERE login = '{$login}' AND senha = '{$senha}'";
+                $resultado = mysqli_query($connect, $sql);
+
+                if (1 == mysqli_num_rows($resultado)) {
+                    // Converte resultado em array e atribui a $dados
+                    $dados = mysqli_fetch_array($resultado);
+                    mysqli_close($connect);
+                    $_SESSION['logado'] = true;
+                    $_SESSION['id_usuario'] = $dados['id'];
+                    header('Location: home.php');
+                } else {
+                    $erros[] = '<li>Usuário e senha não conferem.</li>';
+                }
+            } else {
+                $erros[] = '<li>Usuário inexistente.</li>';
+            }
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if (!empty($erros)) {
+            foreach ($erros as $erro) {
+                echo $erro;
+            }
+        }
+    ?>
+
+    <h1>Login</h1>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        Login: <input type="text" name="login"><br>
+        Senha: <input type="password" name="senha"><br>
+        <button type="submit" name="entrar">Entrar</button>
+    </form>
+</body>
+</html>
